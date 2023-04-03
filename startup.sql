69e79db42a7d4d39015016d448dcaa0d3ffbcaf7
AtelyPham <tinpham1412000@gmail.com>
2023-04-03 08:49:26 +0700

    generate db script

--- startup.sql
+++ startup.sql
@@ -1,88 +1,165 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: mysql_db
+-- Generation Time: Apr 03, 2023 at 01:47 AM
+-- Server version: 8.0.32
+-- PHP Version: 8.1.17
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE DATABASE IF NOT EXISTS hutech_php;
 
 USE hutech_php;
 
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hutech_php`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
 CREATE TABLE `cart` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `userid` varchar(11) NOT NULL,
-  `itemid` int(11) NOT NULL,
-  `quanity` int(11) NOT NULL,
+  `itemid` int NOT NULL,
+  `quanity` int NOT NULL,
   `isActive` tinyint(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `cart` (`id`, `userid`, `itemid`, `quanity`, `isActive`) VALUES
-(11, 'bevy', 3, 6, 0),
-(10, 'bevy', 2, 3, 0),
-(9, 'bevy', 1, 3, 0),
-(8, 'admin', 1, 3, 0),
-(12, 'gune', 2, 9, 0),
-(13, 'admin', 6, 3, 0),
-(14, 'admin', 7, 2, 1),
-(15, 'admin', 5, 1, 1),
-(16, 'admin', 6, 5, 1),
-(17, 'bevy', 5, 10, 1),
-(18, 'admin', 7, 5, 1),
-(19, 'admin', 5, 4, 0),
-(20, 'admin', 8, 7, 1),
-(21, 'admin', 1, 3, 1),
-(22, 'admin', 5, 1, 0),
-(23, 'admin', 7, 2, 1),
-(24, 'admin', 1, 2, 0),
-(25, 'admin', 5, 2, 0),
-(26, 'admin', 5, 3, 0),
-(27, 'admin', 8, 9, 0);
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `username` varchar(200) NOT NULL,
+  `item_id` int NOT NULL,
+  `rating` int NOT NULL,
+  `comment` varchar(1024) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`username`, `item_id`, `rating`, `comment`) VALUES
+('guest', 1, 4, 'It\'s the best in the world!!!!');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
 
 CREATE TABLE `items` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `description` varchar(1024) NOT NULL,
   `unit_price` double NOT NULL,
   `image_url` varchar(150) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `items` (`id`, `name`, `unit_price`, `image_url`) VALUES
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`id`, `name`, `description`, `unit_price`, `image_url`) VALUES
 (1, 'Honda Ridgeline', 'Sports car with sleek lines, white racing stripes, and a powerful engine.', 6000000, 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202017/Magazine-Articles/April/CR-Inline-top-picks-Ridgeline-02-17'),
 (5, 'Tesla V800', 'SUV with tinted windows, leather seats, and top-of-the-line safety features.', 2080000, 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202017/Magazine-Articles/April/CR-Inline-top-picks-Toyota-Yaris-02-17'),
 (6, 'Ford F-150', 'Convertible with a retractable roof, chrome accents, and a high-tech stereo system.', 7000000, 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Magazine/04April/CRM-Cars-Inline-TopTen-Ford-F150-2018-4-18'),
 (7, 'Toyota Highlander', 'Sedan with a fuel-efficient hybrid engine, touchscreen navigation, and panoramic sunroof.', 9000000, 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Magazine/04April/CRM-Cars-Inline-TopTen-Toyota-Highlander-2018-4-18'),
 (8, 'Chevrolet Impala', 'Muscle car with racing-inspired decals, manual transmission, and a throaty exhaust note.', 75620000, 'https://article.images.consumerreports.org/prod/content/dam/CRO%20Images%202018/Magazine/04April/CRM-Cars-inline-TopTen-Chevrolet-Imapla-2018-4-18');
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
 CREATE TABLE `login` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL,
   `username` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `address` varchar(100) NOT NULL,
   `telephone` varchar(12) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `avatar` varchar(200) DEFAULT NULL
+  `avatar` varchar(1024) DEFAULT NULL,
+  `isAdmin` tinyint(1) NOT NULL DEFAULT '0'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO `login` (`id`, `username`, `password`, `address`, `telephone`, `email`) VALUES
-(1, 'srer', 'dfsfg', 'fdgf', 'dg`', 'gsg'),
-(2, 'admin', '123', 'fdhgd', 'dgfj', 'gdfjh'),
-(4, 'sumana', 'kk123', 'hsdl', '078986532', 'kaS@gmail.com'),
-(5, '10', '', '', '', ''),
-(6, 'bevy', '789', 'sdfda', '5656', 'bevylabs@gmail.com'),
-(7, 'solo', '789', 'Sumana paya', '', 'solo@gmail.com'),
-(8, 'gune', 'gune', 'hjkafkj', '89652', 'gune@gmail.com');
+--
+-- Dumping data for table `login`
+--
 
+INSERT INTO `login` (`id`, `username`, `password`, `address`, `telephone`, `email`, `avatar`, `isAdmin`) VALUES
+(2, 'admin', '123', 'fdhgd', 'dgfj', 'gdfjh', 'uploads/280x280.png', 1),
+(9, 'guest', 'guest', 'guest', '123-456-7890', 'guest@guest.guest', 'uploads/pexels-monique-pinto-5488132.jpg', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`username`,`item_id`);
+
+--
+-- Indexes for table `items`
+--
 ALTER TABLE `items`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `login`
+--
 ALTER TABLE `login`
   ADD PRIMARY KEY (`id`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
 ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
+--
+-- AUTO_INCREMENT for table `items`
+--
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
+--
+-- AUTO_INCREMENT for table `login`
+--
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
